fbe7b4637e6a57c34a58c2891a22085ce18c8242
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-08 13:39:08 +0100

    Initial Commit

---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Godwin IE on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
