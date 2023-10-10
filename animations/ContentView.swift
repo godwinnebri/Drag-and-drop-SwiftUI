7d7132f4f53eb7d7e32d875cf4389d6ad500b168
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-10-10 23:14:02 +0100

    card and text drag

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    @State private var helloDragAmount = CGSize.zero
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .background(enabled ? .blue : .red)
+                        .offset(helloDragAmount)
+                        .animation(.default.delay(Double(num)/2), value: helloDragAmount)
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged{helloDragAmount = $0.translation}
+                    .onEnded{_ in
+                        withAnimation {
+                            helloDragAmount = .zero
+                            enabled.toggle()
+                        }
+                    }
+            )
+            ZStack{
+//                LinearGradient(colors: [.purple, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+                Color.gray.opacity(0.2)
+                Text("Drag me 🚀")
+                    .foregroundStyle(.white)
+                    .font(.body)
+            }
+            .frame(width: 300, height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged{dragAmount = $0.translation}
+                    .onEnded{_ in
+                        withAnimation {
+                            dragAmount = .zero
+                            
+                        }
+                    }
+            )
+            //        .animation(.spring(), value: dragAmount)
+        } //vstack
     }
 }
 
